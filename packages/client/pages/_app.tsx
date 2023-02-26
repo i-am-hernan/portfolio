e270bf1ad8567591de287b758a7470971b6bb6dc
Steve Chalco <hschalco@gmail.com>
2023-02-26 12:39:56 -0800

    add google tag manager and linkedin metatags

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -3,18 +3,33 @@ import { store } from "../store";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { lightTheme, darkTheme } from "../components/theme";
 import { Layout } from "../components/Layout";
-
-const theme = createTheme(darkTheme);
+import React from "react";
+const theme = createTheme(lightTheme);
 
 const App = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </Provider>
+    <div>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-0N29V0GV97"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `  window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-0N29V0GV97');`,
+        }}
+      />
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </Provider>
+    </div>
   );
 };
 
